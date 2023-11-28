968f9ea0b57085a6bc4176963df5fb41e26c5dd3
zimfite <katy2007dv@gmail.com>
2023-11-28 15:05:34 +0300

    add

--- 15.py
+++ 15.py
@@ -11,7 +11,9 @@ bot = telebot.TeleBot(os.getenv("TOKEN"))
 def start(massage):
     markut=types.InlineKeyboardMarkup()
     item_1 = types.InlineKeyboardButton(text='Сгенерировать рандомное число', callback_data='random')
-    markut.add (item_1)
+    item_2 = types.InlineKeyboardButton(text='Рандомный напиток', callback_data='vodka')
+    item_3 = types.InlineKeyboardButton(text='Рандомный стикер', callback_data='ctic')
+    markut.add (item_1, item_2, item_3)
     
     bot.send_message(massage.chat.id, "Привет! Этот бот в будущем будет делать великие дела!!! Пока вы можете получить рандомное число до 100. \n Разработала Катюша 🥲", reply_markup=markut)
 
@@ -22,11 +24,36 @@ def callback(call):
         if call.data =="random":
             num = random.randint(1, 100)
             bot.send_message(call.message.chat.id, str(num))
-            markut_1=types.InlineKeyboardMarkup()
-            item_2 = types.InlineKeyboardButton(text='Ещё', callback_data='random')
-            markut_1.add(item_2)
             bot.callback_query_handler(func=lambda call:True)
-            
+        elif call.data =='vodka':
+            a = random.randint(1,5)
+            if a == 1:
+                photo_1 = open('байкал.jpg', 'rb')
+                bot.send_photo(call.message.chat.id, photo_1)
+            elif a == 2:
+                photo_2 = open('драйв.jpeg', 'rb')
+                bot.send_photo(call.message.chat.id, photo_2)
+            elif a == 3:
+                photo_3 = open('липтон.jpg', 'rb')
+                bot.send_photo(call.message.chat.id, photo_3)
+            elif a == 4:
+                photo_4 = open('флешбар.jpg', 'rb')
+                bot.send_photo(call.message.chat.id, photo_4)
+            elif a == 5:
+                photo_5 = open('шампусик.jpg', 'rb')
+                bot.send_photo(call.message.chat.id, photo_5)
+        elif call.data == 'ctic':
+            b=(-2,0)
+            if b == 0:
+                photo_0 = open('стикер1.jpg', 'rb')
+                bot.send_photo(call.message.chat.id, photo_0)
+            elif b ==-1:
+                photo__1 = open('стикер2.jpg', 'rb')
+                bot.send_photo(call.message.chat.id, photo__1)
+            elif b ==-2:
+                photo__2 = open('стикер3.jpg', 'rb')
+                bot.send_photo(call.message.chat.id, photo__2)
+
 @bot.message_handler()
 def start(massage):
     bot.send_message(massage.chat.id,"Что-то пошло не так")
