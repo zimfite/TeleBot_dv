57d399538e2e63c6fb48f47db487db326b644df5
zimfite <katy2007dv@gmail.com>
2023-11-28 13:16:47 +0300

    add

--- 15.py
+++ 15.py
@@ -9,20 +9,24 @@ bot = telebot.TeleBot(os.getenv("TOKEN"))
 
 @bot.message_handler(commands=["start"])
 def start(massage):
-    bot.send_message(massage.chat.id, "Привет! Этот бот в будущем будет делать великие дела!!! Пока вы можете получить рандомное число до 100. \n Разработала Катюша 🥲")
-    markupinline = types.InlineKeyboardMarkup()
-    num = random.randint(1, 100)
-    item_1 = types.InlineKeyboardButton(text='Сгенирировать рандомное число', callback_data='/random')
-
-    markupinline.add(item_1)
-    num = random.randint(1, 100)
-    bot.send_message(massage.chat.id, str(num), reply_markup=markupinline)
+    markut=types.InlineKeyboardMarkup()
+    item_1 = types.InlineKeyboardButton(text='Сгенерировать рандомное число', callback_data='random')
+    markut.add (item_1)
     
-# @bot.message_handler(commands=["random"])
-# def randomize(message):
-#     num = random.randint(1, 100)
-#     bot.send_message(message.chat.id, str(num))
+    bot.send_message(massage.chat.id, "Привет! Этот бот в будущем будет делать великие дела!!! Пока вы можете получить рандомное число до 100. \n Разработала Катюша 🥲", reply_markup=markut)
 
+@bot.callback_query_handler(func=lambda call:True)
+
+def callback(call):
+    if call.message:
+        if call.data =="random":
+            num = random.randint(1, 100)
+            bot.send_message(call.message.chat.id, str(num))
+            markut_1=types.InlineKeyboardMarkup()
+            item_2 = types.InlineKeyboardButton(text='Ещё', callback_data='random')
+            markut_1.add(item_2)
+            bot.callback_query_handler(func=lambda call:True)
+            
 @bot.message_handler()
 def start(massage):
     bot.send_message(massage.chat.id,"Что-то пошло не так")
